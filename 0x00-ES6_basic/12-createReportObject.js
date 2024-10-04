bc64cdf6e610a431e33af27f8e0aaf610b63e400
LindaniMags <lindanimags@gmail.com>
2024-10-04 18:38:29 +0000

    12-createReportObject.js

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,8 @@
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(deps) {
+      return Object.keys(deps).length;
+    },
+  };
+}
