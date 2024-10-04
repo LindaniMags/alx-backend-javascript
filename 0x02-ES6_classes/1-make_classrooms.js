1034d0c7facb3734cbda19cbe75ef05b85ca4cd8
LindaniMags <lindanimags@gmail.com>
2024-10-04 15:36:04 +0000

    1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,8 +1,6 @@
 import ClassRoom from "./0-classroom";
-
-export default function initializeRooms() {
-  const cls1 = new ClassRoom(19);
-  const cls2 = new ClassRoom(20);
-  const cls3 = new ClassRoom(34);
-  return [cls1, cls2, cls3];
-}
+export const initializeRooms = () => [
+  new ClassRoom(19),
+  new ClassRoom(20),
+  new ClassRoom(34),
+];
