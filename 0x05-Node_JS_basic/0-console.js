6064964009b628f3c1c57fe3cf1e82b61f3598fa
LindaniMags <lindanimags@gmail.com>
2024-11-26 20:38:36 +0000

    0-console

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,5 @@
-//prints in STDOUT the string argument
-
-const displayMessage = (data) => console.log(data);
+function displayMessage(data) {
+  console.log(data);
+}
 
 module.exports = displayMessage;
