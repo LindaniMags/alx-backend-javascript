48edacbedd20263dc7949233672105dce990f39b
LindaniMags <lindanimags@gmail.com>
2024-11-26 20:34:55 +0000

    4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,14 +2,11 @@ const http = require('http');
 
 // Create HTTP server
 const app = http.createServer((req, res) => {
-  // Set response header
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-  // Send response body
   res.end('Hello Holberton School!');
 });
 
-// Listen on port 1245
 app.listen(1245, () => {
   console.log('Server running at http://localhost:1245/');
 });
