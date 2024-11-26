e22dcc23b7981176b90ee74d3eae100aa5c9895a
LindaniMags <lindanimags@gmail.com>
2024-11-26 20:57:24 +0000

    http-express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,12 +1,14 @@
 const express = require('express');
-//Initialize express app
 
+// Create Express application
 const app = express();
 
+// Define route for the '/' endpoint
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!\n');
 });
 
+// Start server on port 1245
 app.listen(1245, () => {
   console.log('Server running at http://localhost:1245/');
 });
