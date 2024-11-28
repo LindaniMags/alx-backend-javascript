92b348d446418bd865cf52d2f48c8b862a4ef36b
LindaniMags <lindanimags@gmail.com>
2024-11-28 20:13:45 +0000

    spies

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,16 @@
+const sinon = require("sinon");
+const Utils = require("./utils");
+const { expect } = require("chai");
+const sendPaymentRequestToApi = require("./3-payment");
+
+describe("sendPaymentRequestToApi", () => {
+  it("validate the usage of the Utils function", () => {
+    const spy = sinon.spy(Utils);
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(spy.calculateNumber.calledWith("SUM", 100, 20)).to.be.true;
+    expect(spy.calculateNumber.callCount).to.be.equal(1);
+    spy.calculateNumber.restore();
+  });
+});
