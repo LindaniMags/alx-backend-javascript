b6d1a104a1e7afb54b92d798313a9d730f1c3b05
LindaniMags <lindanimags@gmail.com>
2024-11-28 20:22:23 +0000

    spies

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -2,6 +2,6 @@ const Utils = require("./utils");
 
 const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
   const response = Utils.calculateNumber("SUM", totalAmount, totalShipping);
-  console.log("The total is:", response);
+  console.log(`The total is: ${response}`);
 };
 module.exports = sendPaymentRequestToApi;
