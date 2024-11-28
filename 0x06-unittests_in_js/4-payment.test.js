f17ccdfcbda96e3f2cd9d9ca3751221030d29702
LindaniMags <lindanimags@gmail.com>
2024-11-28 20:48:24 +0000

    stub

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,18 +1,16 @@
 const sinon = require("sinon");
 const Utils = require("./utils");
 const { expect } = require("chai");
-const sendPaymentRequestToApi = require("./3-payment");
+const sendPaymentRequestToApi = require("./4-payment");
 
 describe("sendPaymentRequestToApi", () => {
   it("Stub the function Utils.calculateNumber", () => {
-    const spy = sinon.spy(Utils);
+    const spy = sinon.spy(console);
     const stub = sinon.stub(Utils, "calculateNumber");
-    stub.calledWith("SUM", 100, 20).returns(10);
 
+    stub.returns(10);
     sendPaymentRequestToApi(100, 20);
-
-    expect(spy.calculateNumber.calledWith("SUM", 100, 20)).to.be.true;
-    expect(spy.calculateNumber.callCount).to.be.equal(1);
+    expect(stub.calledWith("SUM", 100, 20)).to.be.true;
     expect(spy.log.calledWith("The total is: 10")).to.be.true;
     stub.restore();
     spy.log.restore();
