5ca8b27fcee000d82782b67d9511b5f04a49ca93
LindaniMags <lindanimags@gmail.com>
2024-11-28 20:51:35 +0000

    stub

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -11,7 +11,9 @@ describe("sendPaymentRequestToApi", () => {
     stub.returns(10);
     sendPaymentRequestToApi(100, 20);
     expect(stub.calledWith("SUM", 100, 20)).to.be.true;
+    expect(stub.callCount).to.be.equal(1);
     expect(spy.log.calledWith("The total is: 10")).to.be.true;
+    expect(spy.log.callCount).to.be.equal(1);
     stub.restore();
     spy.log.restore();
   });
